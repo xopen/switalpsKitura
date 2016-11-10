afa32604aab5f7fc35dca7e172e2dcbee88de681
Ikhsan Assaat <ikhsan.assaat@songkick.com>
2016-11-10 15:13:21 +0100

    merge main

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,15 +4,29 @@ import Foundation
 
 HeliumLogger.use()
 
+import Kitura
+
+// Create a new router
 let router = Router()
 
+// Handle HTTP GET requests to /
 router.get("/") {
-   request, response, next in
-   response.send("Hello, World!")
-   next()
+    request, response, next in
+    response.send("Hello, World!")
+    next()
 }
 
-let port = Int(ProcessInfo.processInfo.environment["PORT"] ?? "8090") ?? 8090
+router.post("/create") {
+    request, response, next in
+    print(request)
+    response.send("ok")
+    next()
+}
 
+// Add an HTTP server and connect it to the router
+let port = Int(ProcessInfo.processInfo.environment["PORT"] ?? "8090") ?? 8090
 Kitura.addHTTPServer(onPort: port, with: router)
+Kitura.run()
+
+// Start the Kitura runloop (this call never returns)
 Kitura.run()
